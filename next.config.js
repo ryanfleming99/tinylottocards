cc55049b76aa0ae73023bf412ad6ff06c61e22e2
Ryan Fleming <ryanfleming93@aol.com>
2024-12-22 16:10:57 +0000

    updated config for nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  basePath: "", // Remove if unnecessary
+  trailingSlash: true // Ensures paths work with static exports
 };
 
 module.exports = nextConfig;
